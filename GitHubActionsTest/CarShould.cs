a52c08abbe41e097287a1c87217cdad782a795bf
martflo <martuxifc03@gmail.com>
2023-03-26 20:49:27 +0200

    Initial commit

---
+++ GitHubActionsTest/CarShould.cs
@@ -0,0 +1,20 @@
+using FluentAssertions;
+
+namespace GitHubActionsTest;
+
+public class CarShould
+{
+    [Fact]
+    public void BeBlackByDefault()
+    {
+        Car car = new Car();
+        car.Color.Should().Be("Black");
+    }
+
+    [Fact]
+    public void BeFail()
+    {
+        Car car = new Car();
+        car.Color.Should().Be("azUGHJKASN");
+    }
+}
